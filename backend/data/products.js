dd35ec6e4bc0fb7d5e060f724da76ce800b5d267
Phillip Choi <38228979+Minsu386@users.noreply.github.com>
2023-08-03 12:06:25 -0400

    Backend Express & initial product routes

---
+++ backend/data/products.js
@@ -0,0 +1,82 @@
+const products = [
+  {
+    _id: '1',
+    name: 'Luffy',
+    image: '/images/luffy01.avif',
+    description:
+      'One piece Luffy Gear 5th Anime Figure',
+    brand: 'OnePiece',
+    category: 'Figure',
+    price: 89.99,
+    countInStock: 10,
+    rating: 4.5,
+    numReviews: 12,
+  },
+  {
+    _id: '2',
+    name: 'Zoro',
+    image: '/images/Zoro01.jpg',
+    description:
+    'This figurine represents the famous character of Zoro, member of the crew of Luffy in the manga One Piece. Zoro is represented holding his 9 swords, ready to fight. The figurine is high quality and measures approximately 35 cm high. The details of the sculpture are neat, with a realistic painting that highlights the different elements of the figure. The nine swords are securely attached to Zoros hands and also feature authentic details.',
+    brand: 'OnePiece',
+    category: 'Figure',
+    price: 599.99,
+    countInStock: 7,
+    rating: 4.0,
+    numReviews: 8,
+  },
+  {
+    _id: '3',
+    name: 'Nami',
+    image: '/images/Nami01.jpg',
+    description:
+    'Inspired by the chapter 710 colorspread of the manga, Epoch Studio presents their Nami 1/4 Scale statue. Nami playfully holds a giant paintbrush while a scroll with lillustrated cherry blossoms seemingly comes to life. Effortlessly balanced on a scrolls, Nami gives that signature wink. Materials: Poly + PU + PVC + PC Product Dimensions: (Est.) H 68cm x W 49cm x L 44cm Product Weight: (Est.) 9.5kg',
+    brand: 'OnePiece',
+    category: 'Figure',
+    price: 929.99,
+    countInStock: 5,
+    rating: 3,
+    numReviews: 12,
+  },
+  {
+    _id: '4',
+    name: 'Obito',
+    image: '/images/Obito.webp',
+    description:
+    'A statue of Obito',
+    brand: 'Naruto',
+    category: 'Figure',
+    price: 399.99,
+    countInStock: 11,
+    rating: 5,
+    numReviews: 12,
+  },
+  {
+    _id: '5',
+    name: 'Itachi',
+    image: '/images/Itachi01.jpg',
+    description:
+    'A statue of Itachi',
+    brand: 'Bleach',
+    category: 'Figure',
+    price: 49.99,
+    countInStock: 7,
+    rating: 3.5,
+    numReviews: 10,
+  },
+  {
+    _id: '6',
+    name: 'Eren',
+    image: '/images/Eren01.webp',
+    description:
+      'A statue of Eren',
+    brand: 'Eren',
+    category: 'Figure',
+    price: 29.99,
+    countInStock: 0,
+    rating: 4,
+    numReviews: 12,
+  },
+]
+
+export default products
