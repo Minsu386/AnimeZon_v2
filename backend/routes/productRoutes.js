059ca76cd45e7c5ed66cea57931a5395fe4f1141
Phillip Choi <38228979+Minsu386@users.noreply.github.com>
2023-08-10 20:06:16 -0400

    error middleware created

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,25 +1,26 @@
-import express from "express";
-import asyncHandler from "../middleware/asyncHandler.js";
-import Product from "../models/productModel.js";
-
+import express from 'express';
 const router = express.Router();
+import asyncHandler from '../middleware/asyncHandler.js';
+import Product from '../models/productModel.js';
 
+router.get(
+  '/',
+  asyncHandler(async (req, res) => {
+    const products = await Product.find({});
+    res.json(products);
+  })
+);
 
-router.get('/', asyncHandler(async (req, res) => {
-  const products = await Product.find({})
-  res.json(products);
-}));
-
-//single product route
-router.get('/:id', asyncHandler(async(req, res) => {
-  const product = await Product.findById(req.params.id);
-
-  if(product) {
-    res.json(product);
-  }
-
-  res.status(404).json({ message: 'Product not found' })
-}));
-
+router.get(
+  '/:id',
+  asyncHandler(async (req, res) => {
+    const product = await Product.findById(req.params.id);
+    if (product) {
+      return res.json(product);
+    }
+    res.status(404);
+    throw new Error('Resource not found');
+  })
+);
 
 export default router;
