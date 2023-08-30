2ec3fccb1def1339410cab9edcb9f31f9dce19fd
Phillip Choi <38228979+Minsu386@users.noreply.github.com>
2023-08-30 12:56:41 -0400

    order routes and controllers created

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ dotenv.config();
 import connectDB from './config/db.js';
 import productRoutes from './routes/productRoutes.js';
 import userRoutes from './routes/userRoutes.js';
+import orderRoutes from './routes/orderRoutes.js';
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 
 const port = process.env.PORT || 5000;
@@ -22,8 +23,11 @@ app.use(express.urlencoded({ extended: true }));
 //  Cookie Parser Middleware
 app.use(cookieParser());
 
+
+// Routes
 app.use('/api/products', productRoutes);
 app.use('/api/users', userRoutes);
+app.use('/api/orders', orderRoutes)
 
 app.get('/', (req, res) => {
   res.send('API is running...');
