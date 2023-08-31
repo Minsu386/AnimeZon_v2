7e788cc19d661e1d005d473e8a6cec860569462d
Phillip Choi <38228979+Minsu386@users.noreply.github.com>
2023-08-31 17:01:08 -0400

    PayPal integrated for checkout

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -2,7 +2,7 @@ import asyncHandler from '../middleware/asyncHandler.js';
 import Order from '../models/orderModel.js';
 
 // @desc    Create new order
-// @route   Post /api/orders
+// @route   POST /api/orders
 // @access  Private
 const addOrderItems = asyncHandler(async (req, res) => {
   const {
@@ -35,26 +35,30 @@ const addOrderItems = asyncHandler(async (req, res) => {
     });
 
     const createdOrder = await order.save();
+
     res.status(201).json(createdOrder);
   }
 });
 
-// @desc    Get Logged in user orders
+// @desc    Get logged in user orders
 // @route   GET /api/orders/myorders
 // @access  Private
 const getMyOrders = asyncHandler(async (req, res) => {
   const orders = await Order.find({ user: req.user._id });
-  res.status(200).json(orders);
+  res.json(orders);
 });
 
-// @desc    Get order by id
+// @desc    Get order by ID
 // @route   GET /api/orders/:id
 // @access  Private
 const getOrderById = asyncHandler(async (req, res) => {
-  const order = await Order.findById(req.params.id).populate('user', 'name email');
+  const order = await Order.findById(req.params.id).populate(
+    'user',
+    'name email'
+  );
 
-  if (order ) {
-    res.status(200).json(order);
+  if (order) {
+    res.json(order);
   } else {
     res.status(404);
     throw new Error('Order not found');
@@ -62,26 +66,43 @@ const getOrderById = asyncHandler(async (req, res) => {
 });
 
 // @desc    Update order to paid
-// @route   Get /api/orders/:id/pay
+// @route   GET /api/orders/:id/pay
 // @access  Private
 const updateOrderToPaid = asyncHandler(async (req, res) => {
-  res.send('update order to paid');
+  const order = await Order.findById(req.params.id);
+
+  if (order) {
+    order.isPaid = true;
+    order.paidAt = Date.now();
+    order.paymentResult = {
+      id: req.body.id,
+      status: req.body.status,
+      update_time: req.body.update_time,
+      email_address: req.body.payer.email_address,
+    };
+
+    const updatedOrder = await order.save();
+
+    res.json(updatedOrder);
+  } else {
+    res.status(404);
+    throw new Error('Order not found');
+  }
 });
 
 // @desc    Update order to delivered
-// @route   Get /api/orders/:id/deliver
+// @route   GET /api/orders/:id/deliver
 // @access  Private/Admin
 const updateOrderToDelivered = asyncHandler(async (req, res) => {
   res.send('update order to delivered');
 });
 
-// @desc    Get all Order
-// @route   Get /api/orders
+// @desc    Get all orders
+// @route   GET /api/orders
 // @access  Private/Admin
 const getOrders = asyncHandler(async (req, res) => {
   res.send('get all orders');
 });
-
 
 export {
   addOrderItems,
@@ -90,4 +111,4 @@ export {
   updateOrderToPaid,
   updateOrderToDelivered,
   getOrders,
-}
+};
