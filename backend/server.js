8de5be38903e89e83a8c97aca57519d0b131b6c9
Phillip Choi <38228979+Minsu386@users.noreply.github.com>
2023-09-19 08:40:24 -0400

    added image upload route

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import path from 'path';
 import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
 dotenv.config();
@@ -6,6 +7,7 @@ import connectDB from './config/db.js';
 import productRoutes from './routes/productRoutes.js';
 import userRoutes from './routes/userRoutes.js';
 import orderRoutes from './routes/orderRoutes.js';
+import uploadRoutes from './routes/uploadRoutes.js';
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 
 const port = process.env.PORT || 5000;
@@ -27,7 +29,11 @@ app.use(cookieParser());
 // Routes
 app.use('/api/products', productRoutes);
 app.use('/api/users', userRoutes);
-app.use('/api/orders', orderRoutes)
+app.use('/api/orders', orderRoutes);
+app.use('/api/upload', uploadRoutes);
+
+const __dirname = path.resolve(); //  sets to current dir
+app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
 
 // Paypal
 app.get('/api/config/paypal', (req, res) =>
